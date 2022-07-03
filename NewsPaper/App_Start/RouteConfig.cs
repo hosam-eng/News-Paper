2139708a783a64293251aa3c6926f31d93b26f5d
hossam <hossamabosetta@gmail.com>
2022-07-03 18:07:51 +0200

    updated

--- NewsPaper/App_Start/RouteConfig.cs
+++ NewsPaper/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NewsPaper
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "register", id = UrlParameter.Optional }
             );
         }
     }
